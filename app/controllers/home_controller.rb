7baf18da6bbf2d58b969638767d3b8205adacd7e
Brian Yencho <byencho@gmail.com>
2013-12-10 20:49:40 -0600

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+
+  def show
+    # static
+  end
+
+  def coming_soon
+    # static
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingWebsite::Application.config.secret_token = '53639a561a4b63fd8da56f1abe2c9027665fd5d735f883ac25069bf37abeb93e2005139a5b2e0242d57056e32c31b015d71402756d30439feca6f67869f8a954'
