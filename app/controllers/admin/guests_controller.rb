92792254a14a2157d93dcc75b6a7c11e587dace5
Brian Yencho <byencho@gmail.com>
2014-05-03 16:06:51 -0500

    Split up "Total Cool" count into age categories on admin page

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,8 +1,19 @@
 class Admin::GuestsController < Admin::AdminController
   def index
     @guests = Guest.all.sort_by(&:updated_at).reverse
+
+    attending_guests = Guest.where(attending: true)
+    attending_adults = attending_guests.where("GUEST_TYPE IN (?)", %w(me guest other))
+    attending_children = attending_guests.where(guest_type: "family")
+    attending_children_0_to_2 = attending_children.where(age: 0..2)
+    attending_children_3_to_12 = attending_children.where(age: 3..12)
+    attending_children_13_plus = attending_children.where("AGE > (?)", 12)
+
     @guest_count = Guest.count
-    @coming_count = Guest.where(attending: true).count
+    @coming_count = attending_guests.count  
+    @coming_count_adults_and_children_13_plus = attending_adults.count + attending_children_13_plus.count
+    @coming_count_children_3_to_12 = attending_children_3_to_12.count
+    @coming_count_children_0_to_2 = attending_children_0_to_2.count
     @not_coming_count = Guest.where(attending: false).count
   end
 end
